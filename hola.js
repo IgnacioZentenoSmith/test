dc27121e95fdb943f74a67ece2b4830f4f28e2f1
Ignacio Zenteno Smith <31903811+IgnacioZentenoSmith@users.noreply.github.com>
2023-01-28 16:04:36 -0300

    holi

--- hola.js
+++ hola.js
@@ -1,1 +1,21 @@
 // holi
+
+function asd(a, b) {
+  if(a > b) {
+    if(a == b) {
+      if(a == "holi") {
+
+      }
+    }
+  }
+}
+
+
+
+function asd(a, b) {
+  if (a <= b) return;
+  if(a != b) return;
+  if(a != "holi") return;
+}
+
+//dfgdfgdfg
